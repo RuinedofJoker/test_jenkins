30959780b33226e3f4a7420231e3226c0b94d0af
gesi <linxuanhui@shushi.pro>
2025-06-06 10:07:01 +0800

    first commit

---
+++ src/test/java/org/joker/test_jenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joker.test_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
